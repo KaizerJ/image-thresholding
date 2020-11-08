6bfb8f9c1d871608e440152efeb1f31cbe9c5903
samus <samus@192.168.0.143>
2020-11-08 12:17:55 +0000

    umbralizar implementado

--- src/main/java/com/mycompany/image/thresholding/MainFrame.java
+++ src/main/java/com/mycompany/image/thresholding/MainFrame.java
@@ -2,16 +2,20 @@ package com.mycompany.image.thresholding;
 
 import java.awt.image.BufferedImage;
 import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.highgui.HighGui;
 import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 
 
 public class MainFrame extends javax.swing.JFrame {
 
     private final JFileChooser fc;
     private Mat currentImage;
+    private Mat originalImage;
 
     /**
      * Creates new form MainFrame
@@ -51,11 +55,11 @@ public class MainFrame extends javax.swing.JFrame {
         lienzo.setLayout(lienzoLayout);
         lienzoLayout.setHorizontalGroup(
             lienzoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 768, Short.MAX_VALUE)
+            .addGap(0, 1024, Short.MAX_VALUE)
         );
         lienzoLayout.setVerticalGroup(
             lienzoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 415, Short.MAX_VALUE)
+            .addGap(0, 768, Short.MAX_VALUE)
         );
 
         fileMenu.setText("Ficheros");
@@ -84,6 +88,11 @@ public class MainFrame extends javax.swing.JFrame {
 
         thresholdingMenuItem.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_U, java.awt.event.InputEvent.CTRL_DOWN_MASK));
         thresholdingMenuItem.setText("Umbralizar");
+        thresholdingMenuItem.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                thresholdingMenuItemActionPerformed(evt);
+            }
+        });
         editMenu.add(thresholdingMenuItem);
 
         menubar.add(editMenu);
@@ -104,15 +113,15 @@ public class MainFrame extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(lienzo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
+                .addComponent(lienzo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(lienzo, javax.swing.GroupLayout.DEFAULT_SIZE, 415, Short.MAX_VALUE)
-                .addContainerGap())
+                .addComponent(lienzo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
@@ -122,11 +131,22 @@ public class MainFrame extends javax.swing.JFrame {
         int result = fc.showOpenDialog(this);
         
         if( result == JFileChooser.APPROVE_OPTION ){
-            this.currentImage = Imgcodecs.imread(
+            this.originalImage = Imgcodecs.imread(
                     fc.getSelectedFile().getAbsolutePath());
-            this.lienzo.setImage((BufferedImage) HighGui.toBufferedImage(currentImage));
+            this.lienzo.setImage((BufferedImage) HighGui.toBufferedImage(originalImage));
         }
     }//GEN-LAST:event_openMenuItemActionPerformed
+
+    private void thresholdingMenuItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_thresholdingMenuItemActionPerformed
+        String res = JOptionPane.showInputDialog(this, "Introduce el valor umbral");
+        try{
+            int umbral = Integer.parseInt(res);
+            this.currentImage = umbralizar(originalImage, umbral);
+            this.lienzo.setImage((BufferedImage) HighGui.toBufferedImage(currentImage));
+        } catch(NumberFormatException e){
+            JOptionPane.showConfirmDialog(this, "introduce un número entero entre 0 y 255", "umbral no válido", JOptionPane.ERROR_MESSAGE);
+        }
+    }//GEN-LAST:event_thresholdingMenuItemActionPerformed
 
     /**
      * @param args the command line arguments
@@ -172,4 +192,19 @@ public class MainFrame extends javax.swing.JFrame {
     private javax.swing.JMenuItem saveMenuItem;
     private javax.swing.JMenuItem thresholdingMenuItem;
     // End of variables declaration//GEN-END:variables
+    
+    private Mat umbralizar(Mat imagen_original, Integer umbral) {
+        // crear dos imágenes en niveles de gris con el mismo
+        // tamaño que la original
+        Mat imagenGris = new Mat(imagen_original.rows(), imagen_original.cols(), CvType.CV_8U);
+        Mat imagenUmbralizada = new Mat(imagen_original.rows(), imagen_original.cols(), CvType.CV_8U);
+        // convierte a niveles de grises la imagen original
+        Imgproc.cvtColor(imagen_original, imagenGris, Imgproc.COLOR_BGR2GRAY);
+        // umbraliza la imagen:
+        // - píxeles con nivel de gris > umbral se ponen a 1
+        // - píxeles con nivel de gris <= umbra se ponen a 0
+        Imgproc.threshold(imagenGris, imagenUmbralizada, umbral, 255, Imgproc.THRESH_BINARY);
+        // se devuelve la imagen umbralizada
+        return imagenUmbralizada;
+    }
 }
