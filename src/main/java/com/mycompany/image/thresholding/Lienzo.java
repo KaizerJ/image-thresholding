63da7fee067647a826ca59ea6951e41296efb6aa
Jonay Suárez <kaizeresp@gmail.com>
2020-11-11 19:04:51 +0000

    Cambios del estilo

--- src/main/java/com/mycompany/image/thresholding/Lienzo.java
+++ src/main/java/com/mycompany/image/thresholding/Lienzo.java
@@ -1,7 +1,10 @@
 package com.mycompany.image.thresholding;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import javax.swing.JPanel;
 
@@ -11,14 +14,21 @@ public class Lienzo extends JPanel{
 
 
     public Lienzo() {
-        this.image = new BufferedImage(1024, 768, BufferedImage.TYPE_INT_RGB);
-        this.setPreferredSize(new Dimension(image.getWidth(), image.getHeight()));
+        this.image = null;//new BufferedImage(1024, 768, BufferedImage.TYPE_INT_RGB);
+        //this.setPreferredSize(new Dimension(image.getWidth(), image.getHeight()));
     }
     
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawImage(image, 0, 0, null);
+        if(image != null){
+            g.drawImage(image, 0, 0, null);
+        } else {
+            g.setColor(Color.GRAY);
+            g.setFont(new Font( "SansSerif", Font.PLAIN, 20 ));
+            g.drawString("Abre una imagen en Fichero > Abrir", 
+                    this.getWidth()/2 - 150, this.getHeight()/2);
+        }
     }
     
     public void setImage(BufferedImage image){
